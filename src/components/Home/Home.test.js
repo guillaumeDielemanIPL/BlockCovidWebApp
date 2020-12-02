0adf001d23695bddbe57b8e3fa8e8cac0cde7de6
guillaumeDielemanIPL <guillaume.dieleman@student.vinci.be>
2020-12-02 13:34:54 +0100

    debug tests

--- src/components/Home/Home.test.js
+++ src/components/Home/Home.test.js
@@ -2,11 +2,13 @@ import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import { render } from "@testing-library/react";
 import Header from "./Header";
+import HomeContent from "./HomeContent";
 
 test("renders content", () => {
   const component = render(
     <div>
       <Header />
+      <HomeContent />
     </div>
   );
 });
