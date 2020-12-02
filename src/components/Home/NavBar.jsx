f4f1dfd2f74510e6030b27b473f0414cd18eb93d
guillaumeDielemanIPL <guillaume.dieleman@student.vinci.be>
2020-12-02 10:06:19 +0100

    debug test pipeline

--- src/components/Home/NavBar.jsx
+++ src/components/Home/NavBar.jsx
@@ -2,7 +2,9 @@ import React from "react";
 const NavBar = () => {
   return (
     <nav className="pt-navbar">
-      <div className="pt-navbar-group pt-align-left">Hello</div>
+      <div className="pt-navbar-group pt-align-left">
+        Hello the pipeline is OK
+      </div>
     </nav>
   );
 };
