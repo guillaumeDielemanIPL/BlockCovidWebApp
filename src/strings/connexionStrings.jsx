47fa78d0cc98fad38e480e5cd3484b0561d0ff71
Auristelle <49188724+Auristelle@users.noreply.github.com>
2020-12-04 09:55:14 +0100

    Merge

--- src/strings/connexionStrings.jsx
+++ src/strings/connexionStrings.jsx
@@ -3,7 +3,7 @@ const CONNEXION_STRINGS = {
     MOT_DE_PASSE: "Mot de passe",
     MAIL: "Adresse mail",
     PLACEHOLDER_MAIL: "jhon.doe@mail.com",
-    SE_CONNECTER: "Se connecter",
+    CONNECTER: "Se connecter",
   };
   
   export default CONNEXION_STRINGS;
