162ec63f0b742771f486e23b494c164b496e83a8
guillaumeDielemanIPL <guillaume.dieleman@student.vinci.be>
2020-12-07 11:20:01 +0100

    correction of component QRCodeGenerator

--- src/components/QRCodeGenerator/QRGenerator.jsx
+++ src/components/QRCodeGenerator/QRGenerator.jsx
@@ -1,31 +1,31 @@
 import React from "react";
-import { Document, Page, StyleSheet } from "@react-pdf/renderer";
-import { PDFDownloadLink } from "@react-pdf/renderer";
-var QRCode = require("qrcode");
+// import { Document, Page, StyleSheet } from "@react-pdf/renderer";
+// import { PDFDownloadLink } from "@react-pdf/renderer";
+// var QRCode = require("qrcode");
 const QRGenerator = () => {
-  const styles = StyleSheet.create({
-    page: {
-      flexDirection: "row",
-      backgroundColor: "#E4E4E4",
-    },
-    section: {
-      margin: 10,
-      padding: 10,
-      flexGrow: 1,
-    },
-  });
+  // const styles = StyleSheet.create({
+  //   page: {
+  //     flexDirection: "row",
+  //     backgroundColor: "#E4E4E4",
+  //   },
+  //   section: {
+  //     margin: 10,
+  //     padding: 10,
+  //     flexGrow: 1,
+  //   },
+  // });
   // let base64Image;
   // QRCode.toDataURL("Hello world on a QR code", function (err, url) {
   //   base64Image = "data:application/pdf" + url.slice(url.indexOf(";"));
   //   console.log(base64Image);
   // });
-  const MyDocument = () => (
-    <Document>
-      <Page size="A4" style={styles.page}>
-        {/* <Image source={{ uri: base64Image }} /> */}
-      </Page>
-    </Document>
-  );
+  // const MyDocument = () => (
+  //   <Document>
+  //     <Page size="A4" style={styles.page}>
+  //       {/* <Image source={{ uri: base64Image }} /> */}
+  //     </Page>
+  //   </Document>
+  // );
   return (
     <div>
       <div>
